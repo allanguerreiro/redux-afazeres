c6ac07a7153e4879aa5c62504bc442d296ecb049
Allan Guerreiro Carneiro <allanguerreiro@gmail.com>
2019-09-12 15:29:01 -0300

    Correcao imports
    FIX: Correcao de imports

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import MainReducer from './reducers/MainReducer'
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, compose } from "redux";
+import MainReducer from "./reducers/MainReducer";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer)
+const store = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore)(MainReducer);
 
-ReactDOM.render(<Provider store={store}>
-<App />
-</Provider>
-, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
